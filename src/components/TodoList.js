f1dfe0073f404fbd46d6fafe076d00cc06d97741
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-12-16 12:21:26 -0800

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+export default class todoList extends Component {
+  render() {
+    return (
+      <ul className='list-group my-5'>
+        <h3 className='text-capitalze text-center'> Todo List</h3>
+        <TodoItem />
+        <button
+          type='button'
+          className='btn btn-danger btn-block text-capitalize
+        mt-5'>
+          Clear List
+        </button>
+      </ul>
+    );
+  }
+}
