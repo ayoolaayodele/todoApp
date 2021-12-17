1a7b696d11fae76f99b488dcbdad480a1d543305
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-12-17 08:48:18 -0800

    Passed props through todoItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,14 +3,25 @@ import TodoItem from './TodoItem';
 
 export default class todoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
       <ul className='list-group my-5'>
         <h3 className='text-capitalze text-center'> Todo List</h3>
-        <TodoItem />
+
+        {items.map((item) => (
+          <TodoItem
+            key={item.id}
+            title={item.title}
+            handleDelete={() => handleDelete(item.id)}
+            handleEdit={() => handleEdit(item.id)}
+          />
+        ))}
+
         <button
           type='button'
           className='btn btn-danger btn-block text-capitalize
-        mt-5'>
+        mt-5'
+          onClick={clearList}>
           Clear List
         </button>
       </ul>
